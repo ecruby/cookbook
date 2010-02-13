5808935839907275102fe127b6ec46228d814dfd
Brian Hogan <brianhogan@napcs.com>
2010-02-12 22:00:30 -0600

    Added categories to the system

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ActiveRecord::Base
   validates_presence_of :name, :ingredients, :instructions
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100213024425) do
+ActiveRecord::Schema.define(:version => 20100213034342) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
@@ -17,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20100213024425) do
     t.text     "instructions"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
