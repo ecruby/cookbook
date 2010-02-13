d5099eb203fab1f73f8eaee4068b02fbd39e694e
Brian Hogan <brianhogan@napcs.com>
2010-02-12 21:27:51 -0600

    validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @number_of_recipes = Recipe.count
+    @latest_recipe = Recipe.last
   end
 
   def about
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,3 @@
 class Recipe < ActiveRecord::Base
+  validates_presence_of :name, :ingredients, :instructions
 end
