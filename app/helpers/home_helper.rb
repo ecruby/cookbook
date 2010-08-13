ff6687e4e5e0bc9104deb6f9bee75b88050611e6
Aaron Godin <godin.aaron@gmail.com>
2010-08-12 23:05:45 -0500

    Fixed the home page when no recipes have been made. Moved latest recipe to a helper method in home_helper.rb. Added 10px margin to top. Added a .gitignore file.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,18 @@
 module HomeHelper
+  def latest_recipe
+    latest_recipe = Recipe.last
+    return "N/A" if latest_recipe.nil?
+    <<-EOF
+    <p>
+      Name: #{latest_recipe.name}
+    </p>
+    <p>Ingredients:<br />
+      #{latest_recipe.ingredients}
+    </p>
+    <p>
+      Instructions:<br />
+      #{latest_recipe.instructions}
+    </p>
+    EOF
+  end
 end
